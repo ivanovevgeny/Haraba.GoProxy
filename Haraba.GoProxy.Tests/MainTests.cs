408f46048d3ebcffff5f702a7f11b82cf34d182b
Evgeny Ivanov <eivanov.mgn@gmail.com>
2023-09-27 16:46:41 +0500

    just testing lib

--- Haraba.GoProxy.Tests/MainTests.cs
+++ Haraba.GoProxy.Tests/MainTests.cs
@@ -1,13 +1,23 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace Haraba.GoProxy.Tests
 {
     public class MainTests
     {
-        private const string GoProxyUrl = "http://localhost:8000/handle";
-        private const string ChromeUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36";
+        private const string GoProxyUrl = "http://localhost:8000/";
+        private const string ChromeUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36";
         private const string ChromeJa3 = "771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,0-23-65281-10-11-35-16-5-13-18-51-45-43-27-21,29-23-24,0";
+        //private const string ChromeJa3 = "1234";
+        //private const string Url = "https://ja3er.com/json";
+        //private const string Url = "https://www.g2.com/";
+        //private const string Url = "https://www.c-and-a.com/img/product/q_auto:good,b_rgb:E0DEDA,c_scale,w_477/v1691581791/productimages/2209221-1-08.jpg";
+        //private const string Url = "https://p5s.ru/upload/iblock/a82/a82d81b72125d6e177f5b38254dcdcc3.png";
+        private const string Url = "https://www.sportsdirect.com/images/products/21181918_l.jpg";
         
         [Test]
         public async Task GetResponse_ShouldApplyJA3()
@@ -15,18 +25,17 @@ namespace Haraba.GoProxy.Tests
             var response = await GoHttpRequest.Create(GoProxyUrl)
                 .WithJa3(ChromeJa3)
                 .WithUserAgent(ChromeUserAgent)
-                .WithHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+                .WithHeader("Accept", "*")
                 .WithHeader("Accept-Language", "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7")
                 .WithHeader("Accept-Encoding", "gzip, deflate, br")
-                .WithHeader("Sec-Fetch-Mode", "navigate")
-                .WithHeader("Sec-Fetch-Dest", "document")
-                .WithHeader("Sec-Fetch-Site", "none")
-                .WithHeader("Sec-Fetch-User", "?1")
-                .WithHeader("Upgrade-Insecure-Requests", "1")
-                .GetResponseAsync("https://ja3er.com/json");
+                .GetResponseAsync(Url);
+            
+            //await File.WriteAllBytesAsync(@"saved.png", Convert.FromBase64String(response.Payload.Text));
+            await File.WriteAllBytesAsync(@"saved.jpg", Convert.FromBase64String(response.Payload.Text));
+            //File.WriteAllText(@"d:\Projects\Study\Haraba.GoProxy-main\g2.html", response.Payload.Text);
             
             Assert.AreEqual(true, response.Success);
-            Assert.IsTrue(response.Payload.Text.Contains(ChromeJa3));
+            //Assert.IsTrue(response.Payload.Text.Contains(ChromeJa3));
         }
 
         [Test]
@@ -38,11 +47,6 @@ namespace Haraba.GoProxy.Tests
                 .WithHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
                 .WithHeader("Accept-Language", "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7")
                 .WithHeader("Accept-Encoding", "gzip, deflate, br")
-                .WithHeader("Sec-Fetch-Mode", "navigate")
-                .WithHeader("Sec-Fetch-Dest", "document")
-                .WithHeader("Sec-Fetch-Site", "none")
-                .WithHeader("Sec-Fetch-User", "?1")
-                .WithHeader("Upgrade-Insecure-Requests", "1")
                 .GetResponse("https://ja3er.com/json");
 
             Assert.AreEqual(true, response.Success);
@@ -58,6 +62,25 @@ namespace Haraba.GoProxy.Tests
             
             Assert.AreEqual(true, response.Success);
             Assert.IsFalse(response.Payload.Text.Contains(ChromeJa3));
+        }
+        
+        [Test]
+        public async Task GetResponse_TestUrl()
+        {
+            var client = new HttpClient();
+
+            try
+            {
+                var response = await client.GetAsync(Url);
+                var responseBody = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseBody?.Take(50));
+                Assert.True(response.IsSuccessStatusCode);
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
         }
     }
 }
